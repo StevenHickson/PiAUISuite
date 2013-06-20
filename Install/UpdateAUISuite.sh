512c372f002796d5cd725df8a00a7a80b41f488f
Steven Hickson <me@stevenhickson.com>
2013-06-20 18:18:47 -0400

    Added functions to Update to specify what you want to update

--- Install/UpdateAUISuite.sh
+++ Install/UpdateAUISuite.sh
@@ -1,5 +1,92 @@
 #!/bin/bash
 
+function playvideo() {
+if [ -e "/usr/bin/playvideo" ] ; then
+    echo "Updating playvideo ..."
+    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/PlayVideoScripts/playvideo
+    chmod +x /usr/bin/playvideo
+    wget -N -P /usr/share/man/man8/ https://raw.github.com/StevenHickson/PiAUISuite/master/PlayVideoScripts/playvideo.8.gz
+fi
+}
+
+function download() {
+if [ -e "/usr/bin/download" ] ; then
+    echo "Updating download ..."
+    tmp="$ROOT_DIR/DownloadController/"
+    tmp+="$DIR"
+    tmp+="download"
+    wget -N -P /usr/bin/ "$tmp"
+    chmod +x /usr/bin/download
+fi
+}
+
+function gtextcommand() {
+if [ -e "/usr/bin/gtextcommand" ] ; then
+    echo "Updating gtextcommand ..."
+    tmp="$ROOT_DIR/TextCommand/"
+    tmp+="$DIR"
+    tmp+="gtextcommand"
+    wget -N -P /usr/bin/ "$tmp"
+    chmod +x /usr/bin/gtextcommand
+fi
+}
+
+function gvapi() {
+if [ -e "/usr/bin/gvapi" ] ; then
+    echo "Updating gvapi ..."
+    tmp="$ROOT_DIR/TextCommand/"
+    tmp+="$DIR"
+    tmp+="gvapi"
+    wget -N -P /usr/bin/ "$tmp"
+    chmod +x /usr/bin/gvapi
+    wget -N -P /usr/share/man/man8/ https://raw.github.com/StevenHickson/PiAUISuite/master/TextCommand/gvapi.8.gz
+fi
+}
+
+function youtube() {
+if [ -e "/usr/bin/youtube" ] ; then
+    echo "Updating youtube ..."
+    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/youtube
+    chmod +x /usr/bin/youtube
+    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/youtube-safe
+    chmod +x /usr/bin/youtube-safe
+    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/youtube-dlfast
+    chmod +x /usr/bin/youtube-dlfast
+    tmp="$ROOT_DIR/Youtube/"
+    tmp+="$DIR"
+    tmp+="youtube-search"
+    wget -N -P /usr/bin/ "$tmp"
+    chmod +x /usr/bin/youtube-search
+    wget -N -P /usr/share/applications/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/yt.desktop
+    wget -N -P /usr/share/applications/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/ytb.desktop
+    mkdir -p "$USER_HOME/.local/share/midori/scripts"
+    wget -N -P "$USER_HOME/.local/share/midori/scripts/" https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/yt.js
+    wget -N -P "$USER_HOME/.local/share/midori/scripts/" https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/ytb.js
+    update-desktop-database
+    youtube-dl -U
+fi
+}
+
+function voicecommand() {
+if [ -e "/usr/bin/voicecommand" ] ; then
+    echo "Updating voicecommand ..."
+    tmp="$ROOT_DIR/VoiceCommand/"
+    tmp+="$DIR"
+    tmp+="voicecommand"
+    wget -N -P /usr/bin/ "$tmp"
+    chmod +x /usr/bin/voicecommand
+    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/google
+    chmod +x /usr/bin/google
+    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/tts
+    chmod +x /usr/bin/tts
+    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/tts-nofill
+    chmod +x /usr/bin/tts-nofill
+    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/speech-recog.sh
+    chmod +x /usr/bin/speech-recog.sh
+    wget -N -P /usr/share/man/man8/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/voicecommand.8.gz
+fi
+}
+
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root" 1>&2
@@ -34,75 +121,24 @@ else
     rm UpdateAUISuite.sh.1
 fi
 
-if [ -e "/usr/bin/playvideo" ] ; then
-    echo "Updating playvideo ..."
-    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/PlayVideoScripts/playvideo
-    chmod +x /usr/bin/playvideo
-    wget -N -P /usr/share/man/man8/ https://raw.github.com/StevenHickson/PiAUISuite/master/PlayVideoScripts/playvideo.8.gz
-fi
-
-if [ -e "/usr/bin/download" ] ; then
-    echo "Updating download ..."
-    tmp="$ROOT_DIR/DownloadController/"
-    tmp+="$DIR"
-    tmp+="download"
-    wget -N -P /usr/bin/ "$tmp"
-    chmod +x /usr/bin/download
-fi
-
-if [ -e "/usr/bin/gtextcommand" ] ; then
-    echo "Updating gtextcommand ..."
-    tmp="$ROOT_DIR/TextCommand/"
-    tmp+="$DIR"
-    tmp+="gtextcommand"
-    wget -N -P /usr/bin/ "$tmp"
-    chmod +x /usr/bin/gtextcommand
-fi
-
-if [ -e "/usr/bin/gvapi" ] ; then
-    echo "Updating gvapi ..."
-    tmp="$ROOT_DIR/TextCommand/"
-    tmp+="$DIR"
-    tmp+="gvapi"
-    wget -N -P /usr/bin/ "$tmp"
-    chmod +x /usr/bin/gvapi
-    wget -N -P /usr/share/man/man8/ https://raw.github.com/StevenHickson/PiAUISuite/master/TextCommand/gvapi.8.gz
-fi
-
-if [ -e "/usr/bin/youtube" ] ; then
-    echo "Updating youtube ..."
-    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/youtube
-    chmod +x /usr/bin/youtube
-    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/youtube-safe
-    chmod +x /usr/bin/youtube-safe
-    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/youtube-dlfast
-    chmod +x /usr/bin/youtube-dlfast
-    tmp="$ROOT_DIR/Youtube/"
-    tmp+="$DIR"
-    tmp+="youtube-search"
-    wget -N -P /usr/bin/ "$tmp"
-    chmod +x /usr/bin/youtube-search
-    wget -N -P /usr/share/applications/ https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/yt.desktop
-    mkdir -p "$USER_HOME/.local/share/midori/scripts"
-    wget -N -P "$USER_HOME/.local/share/midori/scripts/" https://raw.github.com/StevenHickson/PiAUISuite/master/Youtube/yt.js
-    update-desktop-database
-    youtube-dl -U
-fi
-
-if [ -e "/usr/bin/voicecommand" ] ; then
-    echo "Updating voicecommand ..."
-    tmp="$ROOT_DIR/VoiceCommand/"
-    tmp+="$DIR"
-    tmp+="voicecommand"
-    wget -N -P /usr/bin/ "$tmp"
-    chmod +x /usr/bin/voicecommand
-    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/google
-    chmod +x /usr/bin/google
-    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/tts
-    chmod +x /usr/bin/tts
-    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/tts-nofill
-    chmod +x /usr/bin/tts-nofill
-    wget -N -P /usr/bin/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/speech-recog.sh
-    chmod +x /usr/bin/speech-recog.sh
-    wget -N -P /usr/share/man/man8/ https://raw.github.com/StevenHickson/PiAUISuite/master/VoiceCommand/voicecommand.8.gz
+ARG="$1"
+if [ "$ARG" == "playvideo" ] ; then 
+    playvideo
+elif [ "$ARG" == "download" ] ; then 
+    download
+elif [ "$ARG" == "gtextcommand" ] ; then
+    gtextcommand
+elif [ "$ARG" == "gvapi" ] ; then
+    gvapi
+elif [ "$ARG" == "youtube" ] ; then
+    youtube
+elif [ "$ARG" == "voicecommand" ] ; then
+    voicecommand
+else
+    playvideo
+    download
+    gtextcommand
+    gvapi
+    youtube
+    voicecommand
 fi
