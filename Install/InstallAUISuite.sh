d847d6f81f957cd4c2fdd6145e73bf27b4554711
Steven Hickson <me@stevenhickson.com>
2013-06-27 23:49:11 -0400

    Elaborated on Install instructions

--- Install/InstallAUISuite.sh
+++ Install/InstallAUISuite.sh
@@ -2,10 +2,12 @@
 
 #Script to make install of AUI Suite very easy
 function dependencies() {
+    ARCH=$1
     #Ask to install dependencies
     install_method=`pgrep apt-get`
     if [ -z $install_method ] ; then
     echo "Install dependencies? y/n"
+    echo "These are necessary for any of the options, so you should probably press y unless you absolutely know you have them already"
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
         if [ "$ARCH" == "armv6l" ] ; then
@@ -20,6 +22,7 @@ function dependencies() {
 function playvideo() {
     #Install playvideo script
     echo "Install playvideo? y/n"
+    echo "This script indexes your movies and tv shows in order to quickly find, randomize, and/or play them. I find it extremely useful"
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
     echo "Installing playvideo"
@@ -39,6 +42,7 @@ function download() {
     USER_HOME="$2"
     #Install downloader script
     echo "Install downloader? y/n"
+    echo "This script automates the download of torrents. Ex. 'download wheezy' finds and downloads the newest debian wheezy image"
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
     echo "Installing downloader script"
@@ -73,6 +77,7 @@ function gvapi() {
     USER_HOME="$2"
     #Install gvapi
     echo "Install gvapi (googlevoice api)? y/n"
+    echo "This script installs the google voice api. It is really useful for home automation/robotics enthusiasts."
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
         echo "Installing Text Command Script"
@@ -102,6 +107,7 @@ function gtextcommand {
     USER_HOME="$2"
     #Install gtextcommand script
     echo "Install gtextcommand (google voice text command system)? y/n"
+    echo "This installs gtextcommand. This uses google voice to check for system commands from your number with a passcode."
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
     echo "Installing Text Command Script"
@@ -153,6 +159,7 @@ function youtube() {
     DIR=$1
     USER_HOME="$2"
     echo "Install youtube scripts? y/n"
+    echo "This installs youtube, youtube-safe, youtube-dl, and other scripts that allow you to download, stream, and browse videos from many sites"
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
         tmp="../Youtube/"
@@ -179,6 +186,7 @@ function voicecommand() {
     DIR=$1
     USER_HOME="$2"
     echo "Install voicecommand? y/n"
+    echo "This is probably the coolest script here and ties many of these together. It is an easily customizable voice control system. It uses speech recognition and text to speech to listen to you, respond to you, and run commands based on what you say."
     read option
     if [ $option == "y" ] || [ $option == "Y" ] ; then
         tmp="../VoiceCommand/"
@@ -225,7 +233,7 @@ fi
 
 ARG="$1"
 if [ "$ARG" == "dependencies" ] ; then 
-    dependencies
+    dependencies "$ARCH"
 elif [ "$ARG" == "playvideo" ] ; then 
     playvideo
 elif [ "$ARG" == "download" ] ; then 
@@ -239,7 +247,7 @@ elif [ "$ARG" == "youtube" ] ; then
 elif [ "$ARG" == "voicecommand" ] ; then
     voicecommand "$DIR" "$USER_HOME"
 else
-    dependencies
+    dependencies "$ARCH"
     playvideo
     download "$DIR" "$USER_HOME"
     gtextcommand "$DIR" "$USER_HOME"
