a985ca66f4d2c70587bbf7a41f10c77a22a586c5
Steven Hickson <me@stevenhickson.com>
2013-06-27 23:11:42 -0400

    Changed Install and Uninstall script to match Update script to allow program choice

--- Install/InstallAUISuite.sh
+++ Install/InstallAUISuite.sh
@@ -1,6 +1,209 @@
 #!/bin/bash
 
 #Script to make install of AUI Suite very easy
+function dependencies() {
+    #Ask to install dependencies
+    install_method=`pgrep apt-get`
+    if [ -z $install_method ] ; then
+    echo "Install dependencies? y/n"
+    read option
+    if [ $option == "y" ] || [ $option == "Y" ] ; then
+        if [ "$ARCH" == "armv6l" ] ; then
+            sudo apt-get install locate curl libboost1.50-dev libboost-regex1.50-dev xterm xfonts-base xfonts-utils youtube-dl axel mpg123 libcurl4-gnutls-dev flac sox
+        else
+            sudo apt-get install locate curl libboost1.46-dev libboost-regex1.46-dev xterm xfonts-base xfonts-utils youtube-dl axel mpg123 libcurl4-gnutls-dev flac sox
+        fi
+    fi
+    fi
+}
+
+function playvideo() {
+    #Install playvideo script
+    echo "Install playvideo? y/n"
+    read option
+    if [ $option == "y" ] || [ $option == "Y" ] ; then
+    echo "Installing playvideo"
+    echo "Enter movie/video root folder location, ex: /media/External/movies"
+    read media_path
+    `../PlayVideoScripts/playvideo.sh -set "$media_path"`
+    sudo cp ../PlayVideoScripts/playvideo /usr/bin/playvideo
+    sudo cp ../PlayVideoScripts/playvideo.8.gz /usr/share/man/man8/
+    echo "Done installing playvideo!"
+    else
+        echo "Skipping playvideo install"
+    fi
+}
+
+function download() {
+    DIR=$1
+    USER_HOME="$2"
+    #Install downloader script
+    echo "Install downloader? y/n"
+    read option
+    if [ $option == "y" ] || [ $option == "Y" ] ; then
+    echo "Installing downloader script"
+    echo "Enter host: ex, localhost (this is probably what it is)"
+    read host
+    echo "Enter Port: default is 9091 I believe"
+    read port
+    echo "Enter username, press enter if none"
+    read user
+    echo "Enter password, press enter if none"
+    read passwd
+    #print commands to $USER_HOME/.down.info
+    if [ -e "$USER_HOME/.down.info" ] ; then
+        sudo rm -f "$USER_HOME/.down.info"
+    fi
+    echo "$host"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
+    echo "$port"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
+    echo "$user"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
+    echo "$passwd" | sudo tee -a "$USER_HOME/.down.info" >/dev/null
+    tmp="../DownloadController/"
+    tmp+="$DIR"
+    tmp+="download"
+    sudo cp "$tmp" /usr/bin/download
+    echo "Done installing download!"
+    else
+        echo "Skipping downloader install"
+    fi
+}
+
+function gvapi() {
+    DIR=$1
+    USER_HOME="$2"
+    #Install gvapi
+    echo "Install gvapi (googlevoice api)? y/n"
+    read option
+    if [ $option == "y" ] || [ $option == "Y" ] ; then
+        echo "Installing Text Command Script"
+        echo "Enter google voice username: "
+        read user
+        echo "Enter google voice password: "
+        read passwd
+        #print commands to $USER_HOME/.gtext
+        if [ -e "$USER_HOME/.gv" ] ; then
+            sudo rm -f "$USER_HOME/.gv"
+        fi
+        echo "$user"   | sudo tee -a "$USER_HOME/.gv" >/dev/null
+        echo "$passwd"   | sudo tee -a "$USER_HOME/.gv" >/dev/null
+        tmp="../TextCommand/"
+        tmp+="$DIR"
+        tmp+="gvapi"
+        sudo cp "$tmp" /usr/bin/gvapi
+        sudo cp ../TextCommand/gvapi.8.gz /usr/share/man/man8/
+        echo "Done installing gvapi!"
+    else
+        echo "Skipping gvapi install"
+    fi
+}
+
+function gtextcommand {
+    DIR=$1
+    USER_HOME="$2"
+    #Install gtextcommand script
+    echo "Install gtextcommand (google voice text command system)? y/n"
+    read option
+    if [ $option == "y" ] || [ $option == "Y" ] ; then
+    echo "Installing Text Command Script"
+    echo "Enter google voice username: "
+    read user
+    echo "Enter google voice password: "
+    read passwd
+    echo "Enter command keyword, ex: Cmd (this will proceed every vaild command)"
+    read key
+    echo "Enter valid number to accept commands from (make sure to put the country code but not the +) ex: 15553334444"
+    read number
+    #print commands to $USER_HOME/.gtext
+    if [ -e "$USER_HOME/.gtext" ] ; then
+        sudo rm -f "$USER_HOME/.gtext"
+    fi
+    echo "$user"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
+    echo "$passwd"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
+    echo "$key"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
+    echo "$number" | sudo tee -a "$USER_HOME/.gtext" >/dev/null
+    tmp="../TextCommand/"
+    tmp+="$DIR"
+    tmp+="gtextcommand"
+    sudo cp "$tmp" /usr/bin/gtextcommand
+    #Add to cron.d
+    echo "Done installing, setting up cron script ..."
+    if [ -e "/etc/cron.d/gtextcommand" ] ; then
+        sudo rm -f "/etc/cron.d/gtextcommand"
+    fi
+    cronjob="#!/bin/sh
+    #
+    # cron script to check google voice.
+    #
+    # Written by Steven Hickson <me@stevenhickson.com> for the gtextcommand script.
+    #
+    DISPLAY=:0
+
+    * * * * * $USER gtextcommand
+
+    "
+    echo "$cronjob" | sudo tee -a /etc/cron.d/gtextcommand >/dev/null
+    sudo sh -c 'chmod +x /etc/cron.d/gtextcommand'
+    echo "Done installing gtextcommand!"
+    else
+        echo "Skipping gtextcommand install"
+    fi
+}
+
+function youtube() {
+    DIR=$1
+    USER_HOME="$2"
+    echo "Install youtube scripts? y/n"
+    read option
+    if [ $option == "y" ] || [ $option == "Y" ] ; then
+        tmp="../Youtube/"
+        tmp+="$DIR"
+        tmp+="youtube-search"
+        sudo cp ../Youtube/youtube /usr/bin/
+        sudo cp ../Youtube/youtube-safe /usr/bin/
+        sudo cp ../Youtube/youtube-dlfast /usr/bin/
+        sudo cp "$tmp" /usr/bin/
+        sudo cp ../Youtube/yt.desktop /usr/share/applications/
+        sudo cp ../Youtube/ytb.desktop /usr/share/applications/
+        mkdir -p "$USER_HOME/.local/share/midori/scripts"
+        cp ../Youtube/yt.js "$USER_HOME/.local/share/midori/scripts/" 
+        cp ../Youtube/ytb.js "$USER_HOME/.local/share/midori/scripts/" 
+        #This however, I'm fairly certain I need
+        sudo update-desktop-database
+        sudo youtube-dl -U
+    else
+        echo "Skipping youtube install"
+    fi
+}
+
+function voicecommand() {
+    DIR=$1
+    USER_HOME="$2"
+    echo "Install voicecommand? y/n"
+    read option
+    if [ $option == "y" ] || [ $option == "Y" ] ; then
+        tmp="../VoiceCommand/"
+        tmp+="$DIR"
+        tmp+="voicecommand"
+        sudo cp "$tmp" /usr/bin/
+        sudo cp ../VoiceCommand/google /usr/bin/
+        sudo cp ../VoiceCommand/tts /usr/bin/
+        sudo cp ../VoiceCommand/speech-recog.sh /usr/bin/
+        sudo cp ../VoiceCommand/voicecommand.8.gz /usr/share/man/man8/
+        if [ -z "$USER_HOME/.commands.conf" ] ; then
+            cp ../VoiceCommand/commands.conf "$USER_HOME/.commands.conf"
+        fi
+        echo "Would you like voicecommand to try to set itself up? y/n"
+        read option
+        if [ $option == "y" ] || [ $option == "Y" ] ; then
+            voicecommand -s
+        else
+            echo "You can do this manually at any time by running voicecommand -s"
+        fi
+    else
+        echo "Skipping voicecommand install"
+    fi
+}
+
 echo "Installing AUI Suite by Steven Hickson"
 echo "If you have issues, visit stevenhickson.blogspot.com or email me@stevenhickson.com"
 
@@ -20,183 +223,27 @@ else
     USER_HOME="/home/${SUDO_USER}"
 fi
 
-#Ask to install dependencies
-install_method=`pgrep apt-get`
-if [ -z $install_method ] ; then
-echo "Install dependencies? y/n"
-read option
-if [ $option == "y" ] || [ $option == "Y" ] ; then
-    if [ "$ARCH" == "armv6l" ] ; then
-        sudo apt-get install locate curl libboost1.50-dev libboost-regex1.50-dev xterm xfonts-base xfonts-utils youtube-dl axel mpg123 libcurl4-gnutls-dev flac sox
-    else
-        sudo apt-get install locate curl libboost1.46-dev libboost-regex1.46-dev xterm xfonts-base xfonts-utils youtube-dl axel mpg123 libcurl4-gnutls-dev flac sox
-    fi
-fi
-fi
-
-
-#Install playvideo script
-echo "Install playvideo? y/n"
-read option
-if [ $option == "y" ] || [ $option == "Y" ] ; then
-echo "Installing playvideo"
-echo "Enter movie/video root folder location, ex: /media/External/movies"
-read media_path
-`../PlayVideoScripts/playvideo.sh -set "$media_path"`
-sudo cp ../PlayVideoScripts/playvideo /usr/bin/playvideo
-sudo cp ../PlayVideoScripts/playvideo.8.gz /usr/share/man/man8/
-echo "Done installing playvideo!"
+ARG="$1"
+if [ "$ARG" == "dependencies" ] ; then 
+    dependencies
+elif [ "$ARG" == "playvideo" ] ; then 
+    playvideo
+elif [ "$ARG" == "download" ] ; then 
+    download "$DIR" "$USER_HOME"
+elif [ "$ARG" == "gtextcommand" ] ; then
+    gtextcommand "$DIR" "$USER_HOME"
+elif [ "$ARG" == "gvapi" ] ; then
+    gvapi "$DIR" "$USER_HOME"
+elif [ "$ARG" == "youtube" ] ; then
+    youtube "$DIR" "$USER_HOME"
+elif [ "$ARG" == "voicecommand" ] ; then
+    voicecommand "$DIR" "$USER_HOME"
 else
-    echo "Skipping playvideo install"
+    dependencies
+    playvideo
+    download "$DIR" "$USER_HOME"
+    gtextcommand "$DIR" "$USER_HOME"
+    gvapi "$DIR" "$USER_HOME"
+    youtube "$DIR" "$USER_HOME"
+    voicecommand "$DIR" "$USER_HOME"
 fi
-
-#Install downloader script
-echo "Install downloader? y/n"
-read option
-if [ $option == "y" ] || [ $option == "Y" ] ; then
-echo "Installing downloader script"
-echo "Enter host: ex, localhost (this is probably what it is)"
-read host
-echo "Enter Port: default is 9091 I believe"
-read port
-echo "Enter username, press enter if none"
-read user
-echo "Enter password, press enter if none"
-read passwd
-#print commands to $USER_HOME/.down.info
-if [ -e "$USER_HOME/.down.info" ] ; then
-    sudo rm -f "$USER_HOME/.down.info"
-fi
-echo "$host"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
-echo "$port"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
-echo "$user"   | sudo tee -a "$USER_HOME/.down.info" >/dev/null
-echo "$passwd" | sudo tee -a "$USER_HOME/.down.info" >/dev/null
-tmp="../DownloadController/"
-tmp+="$DIR"
-tmp+="download"
-sudo cp "$tmp" /usr/bin/download
-echo "Done installing download!"
-else
-    echo "Skipping downloader install"
-fi
-
-#Install gvapi
-echo "Install gvapi (googlevoice api)? y/n"
-read option
-if [ $option == "y" ] || [ $option == "Y" ] ; then
-echo "Installing Text Command Script"
-echo "Enter google voice username: "
-read user
-echo "Enter google voice password: "
-read passwd
-#print commands to $USER_HOME/.gtext
-if [ -e "$USER_HOME/.gv" ] ; then
-    sudo rm -f "$USER_HOME/.gv"
-fi
-echo "$user"   | sudo tee -a "$USER_HOME/.gv" >/dev/null
-echo "$passwd"   | sudo tee -a "$USER_HOME/.gv" >/dev/null
-tmp="../TextCommand/"
-tmp+="$DIR"
-tmp+="gvapi"
-sudo cp "$tmp" /usr/bin/gvapi
-sudo cp ../TextCommand/gvapi.8.gz /usr/share/man/man8/
-echo "Done installing gvapi!"
-else
-    echo "Skipping gvapi install"
-fi
-
-#Install gtextcommand script
-echo "Install gtextcommand (google voice text command system)? y/n"
-read option
-if [ $option == "y" ] || [ $option == "Y" ] ; then
-echo "Installing Text Command Script"
-echo "Enter google voice username: "
-read user
-echo "Enter google voice password: "
-read passwd
-echo "Enter command keyword, ex: Cmd (this will proceed every vaild command)"
-read key
-echo "Enter valid number to accept commands from (make sure to put the country code but not the +) ex: 15553334444"
-read number
-#print commands to $USER_HOME/.gtext
-if [ -e "$USER_HOME/.gtext" ] ; then
-    sudo rm -f "$USER_HOME/.gtext"
-fi
-echo "$user"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
-echo "$passwd"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
-echo "$key"   | sudo tee -a "$USER_HOME/.gtext" >/dev/null
-echo "$number" | sudo tee -a "$USER_HOME/.gtext" >/dev/null
-tmp="../TextCommand/"
-tmp+="$DIR"
-tmp+="gtextcommand"
-sudo cp "$tmp" /usr/bin/gtextcommand
-#Add to cron.d
-echo "Done installing, setting up cron script ..."
-if [ -e "/etc/cron.d/gtextcommand" ] ; then
-    sudo rm -f "/etc/cron.d/gtextcommand"
-fi
-cronjob="#!/bin/sh
-#
-# cron script to check google voice.
-#
-# Written by Steven Hickson <me@stevenhickson.com> for the gtextcommand script.
-#
-DISPLAY=:0
-
-* * * * * $USER gtextcommand
-
-"
-echo "$cronjob" | sudo tee -a /etc/cron.d/gtextcommand >/dev/null
-sudo sh -c 'chmod +x /etc/cron.d/gtextcommand'
-echo "Done installing gtextcommand!"
-else
-    echo "Skipping gtextcommand install"
-fi
-
-echo "Install youtube scripts? y/n"
-read option
-if [ $option == "y" ] || [ $option == "Y" ] ; then
-    tmp="../Youtube/"
-    tmp+="$DIR"
-    tmp+="youtube-search"
-    sudo cp ../Youtube/youtube /usr/bin/
-    sudo cp ../Youtube/youtube-safe /usr/bin/
-    sudo cp ../Youtube/youtube-dlfast /usr/bin/
-    sudo cp "$tmp" /usr/bin/
-    sudo cp ../Youtube/yt.desktop /usr/share/applications/
-    sudo cp ../Youtube/ytb.desktop /usr/share/applications/
-    mkdir -p "$USER_HOME/.local/share/midori/scripts"
-    cp ../Youtube/yt.js "$USER_HOME/.local/share/midori/scripts/" 
-    cp ../Youtube/ytb.js "$USER_HOME/.local/share/midori/scripts/" 
-    #This however, I'm fairly certain I need
-    sudo update-desktop-database
-    sudo youtube-dl -U
-else
-    echo "Skipping youtube install"
-fi
-
-echo "Install voicecommand? y/n"
-read option
-if [ $option == "y" ] || [ $option == "Y" ] ; then
-    tmp="../VoiceCommand/"
-    tmp+="$DIR"
-    tmp+="voicecommand"
-    sudo cp "$tmp" /usr/bin/
-    sudo cp ../VoiceCommand/google /usr/bin/
-    sudo cp ../VoiceCommand/tts /usr/bin/
-    sudo cp ../VoiceCommand/speech-recog.sh /usr/bin/
-    sudo cp ../VoiceCommand/voicecommand.8.gz /usr/share/man/man8/
-    if [ -z "$USER_HOME/.commands.conf" ] ; then
-        cp ../VoiceCommand/commands.conf "$USER_HOME/.commands.conf"
-    fi
-    echo "Would you like voicecommand to try to set itself up? y/n"
-    read option
-    if [ $option == "y" ] || [ $option == "Y" ] ; then
-        voicecommand -s
-    else
-        echo "You can do this manually at any time by running voicecommand -s"
-    fi
-else
-    echo "Skipping voicecommand install"
-fi
-    
--- Install/UninstallAUISuite.sh
+++ Install/UninstallAUISuite.sh
@@ -2,49 +2,85 @@
 
 #Script to make install of AUI Suite very easy
 
+function playvideo() {
+    USER_HOME="$1"
+    #Uninstall playvideo script
+    echo "Uninstalling playvideo"
+    sudo rm -f /usr/bin/playvideo
+    sudo rm -f "$USER_HOME/.media.db"
+    sudo rm -f /usr/share/man/man8/playvideo.8.gz
+    sudo rm -f /etc/cron.daily/updatelocaldb
+}
+
+function download() {
+    USER_HOME="$1"
+    echo "Uninstalling downloader script"
+    sudo rm -f "$USER_HOME/.down.info"
+    sudo rm /usr/bin/download
+}
+
+function gvapi() {
+    USER_HOME="$1"
+    echo "Uninstalling gvapi script"
+    sudo rm -f "$USER_HOME/.gv"
+    sudo rm -f /usr/bin/gvapi
+    sudo rm -f /usr/share/man/man8/gvapi.8.gz
+}
+
+function gtextcommand {
+    USER_HOME="$1"
+    echo "Uninstalling gtextcommand script"
+    sudo rm -f "$USER_HOME/.gtext"
+    sudo rm -f /usr/bin/gtextcommand
+    sudo rm -f /etc/cron.d/gtextcommand
+}
+
+function youtube() {
+    USER_HOME="$1"
+    echo "Uninstalling youtube scripts"
+    sudo rm -f /usr/bin/youtube
+    sudo rm -f /usr/bin/youtube-safe
+    sudo rm -f /usr/bin/youtube-dlfast
+    sudo rm -f /usr/bin/youtube-search
+    sudo rm -f /usr/share/applications/yt.desktop
+    sudo rm -f /usr/share/applications/ytb.desktop
+    sudo rm -f "$USER_HOME/.local/share/midori/scripts/yt.js"
+    sudo rm -f "$USER_HOME/.local/share/midori/scripts/ytb.js"
+}
+
+function voicecommand() {
+    echo "Uninstalling voicecommand scripts"
+    sudo rm -f /usr/bin/voicecommand
+    sudo rm -f /usr/bin/google
+    sudo rm -f /usr/bin/tts
+    sudo rm -f /usr/bin/speech-recog.sh
+    sudo rm -f /usr/share/man/man8/voicecommand.8.gz
+}
+
 if [ "$(id -u)" != "0" ]; then
     USER_HOME="$HOME"
 else
     USER_HOME="/home/${SUDO_USER}"
 fi
 
-#Ask to install dependencies
-
-#Uninstall playvideo script
-echo "Uninstalling playvideo"
-sudo rm -f /usr/bin/playvideo
-sudo rm -f "$USER_HOME/.media.db"
-sudo rm -f /usr/share/man/man8/playvideo.8.gz
-sudo rm -f /etc/cron.daily/updatelocaldb
-
-echo "Uninstalling downloader script"
-sudo rm -f "$USER_HOME/.down.info"
-sudo rm /usr/bin/download
-
-echo "Uninstalling gvapi script"
-sudo rm -f "$USER_HOME/.gv"
-sudo rm -f /usr/bin/gvapi
-sudo rm -f /usr/share/man/man8/gvapi.8.gz
-
-echo "Uninstalling gtextcommand script"
-sudo rm -f "$USER_HOME/.gtext"
-sudo rm -f /usr/bin/gtextcommand
-sudo rm -f /etc/cron.d/gtextcommand
-
-echo "Uninstalling youtube scripts"
-sudo rm -f /usr/bin/youtube
-sudo rm -f /usr/bin/youtube-safe
-sudo rm -f /usr/bin/youtube-dlfast
-sudo rm -f /usr/bin/youtube-search
-sudo rm -f /usr/share/applications/yt.desktop
-sudo rm -f /usr/share/applications/ytb.desktop
-sudo rm -f "$USER_HOME/.local/share/midori/scripts/yt.js"
-sudo rm -f "$USER_HOME/.local/share/midori/scripts/ytb.js"
-
-echo "Uninstalling voicecommand scripts"
-sudo rm -f /usr/bin/voicecommand
-sudo rm -f /usr/bin/google
-sudo rm -f /usr/bin/tts
-sudo rm -f /usr/bin/speech-recog.sh
-sudo rm -f /usr/share/man/man8/voicecommand.8.gz
-
+ARG="$1"
+if [ "$ARG" == "playvideo" ] ; then 
+    playvideo "$USER_HOME"
+elif [ "$ARG" == "download" ] ; then 
+    download "$USER_HOME"
+elif [ "$ARG" == "gtextcommand" ] ; then
+    gtextcommand "$USER_HOME"
+elif [ "$ARG" == "gvapi" ] ; then
+    gvapi "$USER_HOME"
+elif [ "$ARG" == "youtube" ] ; then
+    youtube "$USER_HOME"
+elif [ "$ARG" == "voicecommand" ] ; then
+    voicecommand "$USER_HOME"
+else
+    playvideo "$USER_HOME"
+    download "$USER_HOME"
+    gtextcommand "$USER_HOME"
+    gvapi "$USER_HOME"
+    youtube "$USER_HOME"
+    voicecommand "$USER_HOME"
+fi
